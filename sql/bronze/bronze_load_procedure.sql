f86dd928f6638edfd5bacc6096a04989441ef42a
HandersThe <38912995+HandersThe@users.noreply.github.com>
2025-03-03 10:30:32 +0700

    ddl and transform to silver layer

--- sql/bronze/bronze_load_procedure.sql
+++ sql/bronze/bronze_load_procedure.sql
@@ -16,6 +16,10 @@ Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
+
+USE DataWarehouse;
+GO
+
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
